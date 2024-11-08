b5755a5e386dbf8f52b5bae5f94474786bc49c99
Samuel Delabranche <samuel.delabranche0@gmail.com>
2024-11-08 23:00:47 +0100

    Enregistrement des modifications locales

--- src/QuestionnaireApp.java
+++ src/QuestionnaireApp.java
@@ -3,10 +3,6 @@ import Logic.GestionnaireDossiers;
 public class QuestionnaireApp {
     public static void main(String[] args) {
         GestionnaireDossiers _gestionnaireDossiers = new GestionnaireDossiers();
-        String[] currentProjects = _gestionnaireDossiers.getCurrentProjectsNames();
-
-        for (String currentProject : currentProjects) {
-            System.out.println(currentProject);
-        }
+        
     }
 }
